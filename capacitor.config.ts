7572d76ff38e2cd0d079b5dfcc0b865e259a1108
Miguel Fernandez <miguelfepa@gmail.com>
2022-02-21 10:17:21 +0100

    cambios clase d el lunes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PC3-test1-Miguel',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
